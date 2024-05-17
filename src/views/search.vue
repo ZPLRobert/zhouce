ca74e43d47936eabf6bb5a651dae04c96047b0ac
ZPLRobert <157563780+ZPLRobert@users.noreply.github.com>
2024-05-17 18:00:13 +0800

    3.0

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,17 +1,17 @@
 <template>
   <div>search</div>
-  <input type="text" :placeholder="defaultKey" />
+  <!-- <input type="text" :placeholder="defaultKey" />
   <van-button @click="runSearchResult({ keywords: '海阔' })">搜索</van-button>
   <ul>
     <li v-for="song in songs" :key="song.id">{{ song.name }}</li>
-  </ul>
+  </ul> -->
 </template>
 <script setup lang="ts">
-import { getDefaultKey, searchResult } from "@/service";
-import { useRequest } from "vue-request";
+// import { getDefaultKey, searchResult } from "@/service";
+// import { useRequest } from "vue-request";
 
-const { data: defaultKey } = useRequest(getDefaultKey, {});
-const { data: songs, run: runSearchResult } = useRequest(searchResult, {
-  manual: true,
-});
+// const { data: defaultKey } = useRequest(getDefaultKey, {});
+// const { data: songs, run: runSearchResult } = useRequest(searchResult, {
+//   manual: true,
+// });
 </script>
